148b57bcb6c8361fd2d50b7e88642cdd05cb603f
Francisco Cunha <afrancisco97@gmail.com>
2022-08-18 14:50:34 +0100

    change in instance

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "name1" {
   vpc_security_group_ids = [aws_security_group.sg_jumper.id]
   associate_public_ip_address = "true"
   tags = {
-    Name = "jumper1"
+    Name = "jumper11"
 
   }
 }
